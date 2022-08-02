0916cdf88d8b236b092b7da7e1ff4a32780d79e2
Sergey <khachikyan.sergey@mail.ru>
2022-08-03 02:38:41 +0400

    first commit

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+    HomeHeader,
+    NFTCard,
+    SubInfo,
+    FocusedStatusBar,
+    CircleButton,
+    DetailsDesc,
+    DetailsBid,
+}
